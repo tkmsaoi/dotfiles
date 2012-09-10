5f001c3ee94bfac2ba5827e64db210d811d6b379
Takamasa Aoi <tkms.aoi@gmail.com>
2012-09-10 14:37:48 +0900

    Update for emacs -nw

--- _emacs.d/inits/10_view.el
+++ _emacs.d/inits/10_view.el
@@ -1,12 +1,15 @@
 (setq inhibit-startup-message t)
 (setq frame-title-format (format "%%f" (system-name)))
-(tool-bar-mode -1)
 (menu-bar-mode -1)
-(set-scroll-bar-mode 'right)
 (line-number-mode t)
 (column-number-mode t)
 (setq resize-mini-windows nil)
 (setq visible-bell t)
+
+(when window-system
+  (progn
+    (tool-bar-mode -1)
+    (set-scroll-bar-mode 'right)))
 
 (add-to-list 'default-frame-alist '(foreground-color . "white"))
 (add-to-list 'default-frame-alist '(background-color . "black"))
