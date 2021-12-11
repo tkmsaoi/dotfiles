7d7493b6eb73c9f916c8c498cf4138eeb94df6ba
Takamasa Aoi <tkms.aoi@gmail.com>
2021-12-11 13:10:59 +0900

    starship 関連修正

--- _config/fish/config.fish
+++ _config/fish/config.fish
@@ -1,11 +1,13 @@
 if status is-interactive
-    set fish_greeting
+  set fish_greeting
 
-    alias g=git
-    alias d=docker
-    alias dc=docker-compose
-    alias pbc=pbcopy
-    alias pbp=pbpaste
+  alias g=git
+  alias d=docker
+  alias dc=docker-compose
+  alias pbc=pbcopy
+  alias pbp=pbpaste
 
+  if type -q starship
     starship init fish | source
+  end
 end
