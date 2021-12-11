dcfde2919efa9392d9c05b83ec8706bb26678d3f
Takamasa Aoi <tkms.aoi@gmail.com>
2021-12-11 12:41:21 +0900

    install.sh 改修

--- install.sh
+++ install.sh
@@ -1,16 +1,42 @@
-#!/bin/sh
-if [ "`uname`" = "Linux" ]; then
-    LN_OPTS=-sfn
+#!/usr/bin/env bash
+
+set -eu
+
+BASE_DIR="$(cd $(dirname $0); pwd)"
+INSTALL_DIR="$HOME"
+
+if [ "$(uname)" = "Linux" ]; then
+  LN_CMD='ln -sfn'
 else
-    LN_OPTS=-sfh
+  LN_CMD='ln -sfh'
 fi
 
-for i in _*
-do
-    source="$PWD/$i"
-    target="$HOME/.${i#_}"
-    if [ -e "$target" ] && [ ! -L "$target" ]; then
-        mv $target $target.`date '+%Y%m%d%H%M%S'`.bak
-    fi
-    ln $LN_OPTS $source $target
+install () {
+  local source="$1"
+  local dest="$2"
+
+  if [ -d "$source" ]; then
+    echo "D $dest"
+    mkdir -p "$dest"
+
+    local file
+    for file in "$source"/*; do
+      local basename="$(basename $file)"
+      install "$file" "$dest/$basename"
+    done
+    return
+  fi
+
+  if [ -e "$dest" ] && [ ! -L "$dest" ]; then
+    echo "B $dest"
+    mv "$dest" "$dest.$(date '+%Y%m%d%H%M%S').bak"
+  fi
+
+  echo "L $dest"
+  $LN_CMD "$source" "$dest"
+}
+
+for file in "$BASE_DIR"/_*; do
+  basename="$(basename $file)"
+  install "$file" "$INSTALL_DIR/.${basename#_}"
 done
