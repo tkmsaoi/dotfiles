58339fd3f96691a9bc7a47a96b4c5f243916e8f7
Takamasa Aoi <tkms.aoi@gmail.com>
2021-12-13 15:35:36 +0900

    config.fish 整理

--- _config/fish/config.fish
+++ _config/fish/config.fish
@@ -1,3 +1,30 @@
+#
+# Environment
+#---
+
+fish_add_path /usr/local/bin
+
+if type -q brew
+  fish_add_path (brew --prefix)/bin
+  fish_add_path (brew --prefix)/sbin
+end
+
+fish_add_path $HOME/local/bin
+
+#
+# asdf
+#---
+
+if type -q asdf
+  if type -q brew
+    source (brew --prefix asdf)/libexec/asdf.fish
+  end
+end
+
+#
+# Interactive Setup
+#---
+
 if status is-interactive
   set fish_greeting
 
@@ -11,6 +38,8 @@ if status is-interactive
     starship init fish | source
   end
 end
+
+#---
 
 if test -e ~/.config/fish/config.fish.local
   source ~/.config/fish/config.fish.local
